66b76f84d037f3fc25006cabf435bc5d7ae00d7b
alexander-steingold <steingold.alex@gmail.com>
2023-09-06 23:03:36 +0300

    refactor

--- routes/web.php
+++ routes/web.php
@@ -29,9 +29,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-//Route::get('/', fn() => to_route('admin.dashboard'));
+Route::get('/', fn() => to_route('admin.dashboard'));
 Route::controller(LandingController::class)->group(function () {
-    Route::get('/', 'index')->name('index');
+    Route::get('/site', 'index')->name('index');
 
 });
 
