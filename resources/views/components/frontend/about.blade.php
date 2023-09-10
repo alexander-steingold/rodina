e5e31dad7ff19e58c13e3a9f9b8900e1c7ac6194
alexander-steingold <steingold.alex@gmail.com>
2023-09-10 14:22:00 +0300

    video refactor

--- resources/views/components/frontend/about.blade.php
+++ resources/views/components/frontend/about.blade.php
@@ -1,25 +1,24 @@
 <!-- about area start -->
 <a name="about"></a>
-<div class="about-area-2 pd-top-60 pd-bottom-180">
+<div class="about-area-2 pd-top-60 pd-bottom-160">
     <div class="container">
         <div class="about-area-inner">
             <div class="row">
-                <div class="col-lg-5 order-lg-2">
-                    <div class="about-thumb-wrap mb-lg-0 mb-4">
-                        <img class="img-1" src="{{ asset('landing/assets/img/about/shape.png') }}" alt="img">
-                        <img class="img-2" src="{{ asset('landing/assets/img/about/7.png') }}" alt="img">
-                        <img class="img-3" src="{{ asset('landing/assets/img/about/8.png') }}" alt="img">
-                        <div class="exprience-wrap">
-                            <img src="{{ asset('landing/assets/img/about/shape-3.png') }}" alt="img">
-                            <div class="details">
-                                <h1>25</h1>
-                                <p>ЛЕТ НА РЫНКЕ</p>
-                            </div>
-                        </div>
+                <div class="col-lg-6 order-lg-2">
+                    <div class="video-thumb-wrap pt-1 pb-4">
+                        <img src="https://i3.ytimg.com/vi/VNbsCTqawBs/maxresdefault.jpg" alt="img">
+                        <a class="video-play-btn" href="https://www.youtube.com/embed/Wimkqo8gDZ0"
+                           data-effect="mfp-zoom-in">
+                            <svg class="svg-inline--fa fa-play fa-w-14" aria-hidden="true" focusable="false"
+                                 data-prefix="fa" data-icon="play" role="img" xmlns="http://www.w3.org/2000/svg"
+                                 viewBox="0 0 448 512" data-fa-i2svg="">
+                                <path fill="currentColor"
+                                      d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path>
+                            </svg><!-- <i class="fa fa-play"></i> Font Awesome fontawesome.com --></a>
                     </div>
                 </div>
-                <div class="col-lg-7 align-self-center order-lg-1">
-                    <div class="about-inner-wrap ms-0">
+                <div class="col-lg-6 align-self-center order-lg-1">
+                    <div class="ms-0">
                         <div class="section-title mb-0">
                             <h6 class="text-danger">О НАС</h6>
                             <h5 class="mt-4 mb-2">
@@ -58,15 +57,15 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="btn-wrap">
-                                {{--                                <a class="btn btn-base" href="about.html">Узнать Больше</a>--}}
-                                <div class="video-wrap">
-                                    <a class="video-play-btn"
-                                       href="https://www.youtube.com/embed/VNbsCTqawBs"
-                                       data-effect="mfp-zoom-in"><i class="fa fa-play"></i></a>
-                                    <span>Смотреть Видео</span>
-                                </div>
-                            </div>
+                            {{--                            <div class="btn-wrap">--}}
+                            {{--                                --}}{{--                                <a class="btn btn-base" href="about.html">Узнать Больше</a>--}}
+                            {{--                                <div class="video-wrap">--}}
+                            {{--                                    <a class="video-play-btn"--}}
+                            {{--                                       href="https://www.youtube.com/embed/VNbsCTqawBs"--}}
+                            {{--                                       data-effect="mfp-zoom-in"><i class="fa fa-play"></i></a>--}}
+                            {{--                                    <span>Смотреть Видео</span>--}}
+                            {{--                                </div>--}}
+                            {{--                            </div>--}}
                         </div>
                     </div>
                 </div>
