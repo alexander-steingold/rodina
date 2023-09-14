5012c3d9440024d4b6a0787f3a02d5fb02242bbd
alexander-steingold <steingold.alex@gmail.com>
2023-09-14 09:38:13 +0300

    add cities by country and code refactoring

--- app/Http/Controllers/Backend/CourierController.php
+++ app/Http/Controllers/Backend/CourierController.php
@@ -25,7 +25,7 @@ class CourierController extends Controller
     public function index()
     {
         $couriers = $this->courierService->index();
-        $cities = City::where('country', 'IL')
+        $cities = City::where('country', 'IL');
         return view('backend.courier.index',
             [
                 'couriers' => $couriers,
@@ -40,7 +40,7 @@ class CourierController extends Controller
      */
     public function create()
     {
-        $cities = City::where('country', 'IL')
+        $cities = City::where('country', 'IL');
         return view('backend.courier.create', ['cities' => $cities]);
     }
 
@@ -69,7 +69,7 @@ class CourierController extends Controller
      */
     public function edit(Courier $courier)
     {
-        $cities = City::where('country', 'IL')
+        $cities = City::where('country', 'IL');
         return view('backend.courier.edit', ['courier' => $courier, 'cities' => $cities]);
     }
 
