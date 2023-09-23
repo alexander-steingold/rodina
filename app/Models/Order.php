6abd53e9b03d969ce51dd58be1d6c181a447c888
alexander-steingold <steingold.alex@gmail.com>
2023-09-23 21:14:58 +0300

    order status refactor

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -11,6 +11,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\Facades\DB;
 
 class Order extends Model
 {
@@ -159,21 +160,98 @@ class Order extends Model
     }
 
 
+//    public function scopeFilter(Builder|QueryBuilder $query, array $filters)
+//    {
+//
+//        // logger('info', [$filters]);
+//        $filterColumns = [
+//            'total_payment' => '>=',
+//            'courier_id' => '=',
+//        ];
+//
+//        $query->when($filters['status'] ?? null, function ($query, $status) {
+//            $query->whereHas('currentStatus', function ($query) use ($status) {
+//                $query->where('status', '=', $status);
+//            });
+//        });
+//
+//        foreach ($filterColumns as $filter => $operator) {
+//            $value = $filters[$filter] ?? null;
+//            $query->when($value, function ($query) use ($filter, $operator, $value) {
+//                if (is_callable($operator)) {
+//                    $operator($query, $value);
+//                } else {
+//                    $query->where($filter, $operator, $value);
+//                }
+//            });
+//        }
+//
+//        $query->when($filters['date_range'] ?? null, function ($query, $dateRange) {
+//            $dates = explode(__('general.to'), $dateRange);
+//            if (count($dates) === 2) {
+//                [$startDate, $endDate] = $dates;
+//                $query->whereDate('created_at', '>=', $startDate)
+//                    ->whereDate('created_at', '<=', $endDate);
+//            }
+//        });
+//
+//        $currentYear = Carbon::now()->year;
+//        $query->when($filters['months'] ?? null, function ($query, $months) use ($currentYear) {
+//            $query->where(function ($query) use ($months, $currentYear) {
+//                foreach ($months as $month) {
+//                    $query->orWhere(function ($query) use ($month, $currentYear) {
+//                        $query->whereMonth('created_at', $month)
+//                            ->whereYear('created_at', $currentYear);
+//                    });
+//                }
+//            });
+//        });
+//
+//
+//        $query->when($filters['search'] ?? null, function ($query, $search) {
+//            $query->where(function ($query) use ($search) {
+//                $query->where('first_name', 'like', '%' . $search . '%')
+//                    ->orWhere('last_name', 'like', '%' . $search . '%')
+//                    ->orWhere('address', 'like', '%' . $search . '%')
+//                    ->orWhere('email', 'like', '%' . $search . '%')
+//                    ->orWhere('phone', 'like', '%' . $search . '%')
+//                    ->orWhere('mobile', 'like', '%' . $search . '%')
+//                    ->orWhere('oid', 'like', '%' . $search . '%')
+//                    ->orWhere('created_at', 'like', '%' . $search . '%')
+//                    ->orWhere('barcode', 'like', '%' . $search . '%')
+//                    ->orWhere('remarks', 'like', '%' . $search . '%')
+//                    ->orWhereHas('customer', function ($query) use ($search) {
+//                        $query->where('first_name', 'like', '%' . $search . '%')
+//                            ->orWhere('last_name', 'like', '%' . $search . '%')
+//                            ->orWhere('address', 'like', '%' . $search . '%');
+//                    });
+//
+//
+//            });
+//        });
+//
+//    }
+
     public function scopeFilter(Builder|QueryBuilder $query, array $filters)
     {
-
-        // logger('info', [$filters]);
+        // Define filter columns and their corresponding operators
         $filterColumns = [
             'total_payment' => '>=',
             'courier_id' => '=',
         ];
 
-        $query->when($filters['status'] ?? null, function ($query, $status) {
-            $query->whereHas('currentStatus', function ($query) use ($status) {
-                $query->where('status', '=', $status);
-            });
-        });
+        // Apply the "status" filter if it is provided
+        if (isset($filters['status'])) {
+            $status = $filters['status'];
 
+            $query->join('order_statuses as latest_statuses', function ($join) use ($status) {
+                $join->on('orders.id', '=', 'latest_statuses.order_id')
+                    ->where('latest_statuses.status', '=', $status)
+                    ->whereRaw('latest_statuses.id = (SELECT MAX(id) FROM order_statuses WHERE order_id = orders.id)');
+            });
+        }
+
+        // Apply other filters based on filter columns and operators
         foreach ($filterColumns as $filter => $operator) {
             $value = $filters[$filter] ?? null;
             $query->when($value, function ($query) use ($filter, $operator, $value) {
@@ -185,6 +263,7 @@ class Order extends Model
             });
         }
 
+        // Apply date range filter
         $query->when($filters['date_range'] ?? null, function ($query, $dateRange) {
             $dates = explode(__('general.to'), $dateRange);
             if (count($dates) === 2) {
@@ -194,6 +273,7 @@ class Order extends Model
             }
         });
 
+        // Apply month filter
         $currentYear = Carbon::now()->year;
         $query->when($filters['months'] ?? null, function ($query, $months) use ($currentYear) {
             $query->where(function ($query) use ($months, $currentYear) {
@@ -206,7 +286,7 @@ class Order extends Model
             });
         });
 
-
+        // Apply search filter
         $query->when($filters['search'] ?? null, function ($query, $search) {
             $query->where(function ($query) use ($search) {
                 $query->where('first_name', 'like', '%' . $search . '%')
@@ -224,12 +304,8 @@ class Order extends Model
                             ->orWhere('last_name', 'like', '%' . $search . '%')
                             ->orWhere('address', 'like', '%' . $search . '%');
                     });
-
-
             });
         });
-
     }
-
 
 }
